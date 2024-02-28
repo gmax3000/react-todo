87df30f9c4041bf09ac44ebc346beacd12de7e03
MM <gmax3000@gmail.com>
2024-02-27 19:17:22 -0800

    update after review lesson_1_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const todoList = [
@@ -10,7 +7,6 @@ const todoList = [
 ];
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
