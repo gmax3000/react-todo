dc7a74709fbe441aff216fe6f96a0f888d23114f
MM <gmax3000@gmail.com>
2024-04-02 11:57:58 +1100

    update handel title change func

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,39 +1,38 @@
 import React, { useState } from 'react';
 
-function AddTodoForm({ onAddTodo }) {
-  const [todoTitle, setTodoTitle] = useState('');
+ function AddTodoForm({ onAddTodo }) {
+   const [todoTitle, setTodoTitle] = useState('');
 
-  const handleTitleChange = (event) => {
-    setTodoTitle(event.target.value);
-  };
+   const handleTitleChange = (event) => 
+   { const newTodoTitle = event.target.value; setTodoTitle(newTodoTitle); };
 
-  const handleAddTodo = (event) => {
-    event.preventDefault();
-    const newTodoTitle = todoTitle; // Storing todoTitle in new variable
-    const newTodo = {
-        title: newTodoTitle,
-        id: Date.now() // Generate a unique identifier
-      };
-      console.log('New Todo:', newTodo); // Access newTodo object
-      console.log('Form submitted!');
-      onAddTodo(newTodo); // Pass newTodo object
-      setTodoTitle(''); // Reset todoTitle state after submission
-  };
+   const handleAddTodo = (event) => {
+     event.preventDefault();
+     const newTodo = {
+         title: todoTitle,
+         id: Date.now() // Generate a unique identifier
+       };
+       console.log('New Todo:', newTodo); // Access newTodo object
+       console.log('Form submitted!');
+       onAddTodo(newTodo); // Pass newTodo object
+       setTodoTitle(''); // Reset todoTitle state after submission
+     // event.target.reset();
+   };
 
-  return (
-    <form onSubmit={handleAddTodo}>
-      <label htmlFor="todoTitle">Title</label>
-      <input
-        type="text"
-        name="title"
-        id="todoTitle"
-        placeholder="Enter your todo"
-        value={todoTitle} // Bind input value to todoTitle state
-        onChange={handleTitleChange} // Update todoTitle state on input change
-      />
-      <button type="submit">Add</button>
-    </form>
-  );
-}
+   return (
+     <form onSubmit={handleAddTodo}>
+       <label htmlFor="todoTitle">Title</label>
+       <input
+         type="text"
+         name="title"
+         id="todoTitle"
+         placeholder="Enter your todo"
+         value={todoTitle} // Bind input value to todoTitle state
+         onChange={handleTitleChange} // Update todoTitle state on input change
+       />
+       <button type="submit">Add</button>
+     </form>
+   );
+ }
 
-export default AddTodoForm;
+ export default AddTodoForm;
