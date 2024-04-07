6f55a91dbe8363007029cbed8c324c718c6bc5ab
MM <gmax3000@gmail.com>
2024-04-07 15:13:55 -0700

    add custom hook_useSemiPersistantState

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 import React, { useState, useEffect } from 'react';
 
-function App() {
-  const [newTodo, setNewTodo] = useState('');
+const useSemiPersistentState = () => {
   const [todoList, setTodoList] = useState(() => {
     // Initialize todoList state by reading from localStorage
     const savedTodoList = localStorage.getItem("savedTodoList");
     return savedTodoList ? JSON.parse(savedTodoList) : [];
   });
-
-  const addTodo = (newTodo) => {
-    setTodoList(prevTodoList => [...prevTodoList, newTodo]);
-  };
-
   useEffect(() => {
-    // This effect will run whenever todoList changes
-    // You can perform any side effects related to todoList here
     localStorage.setItem("savedTodoList", JSON.stringify(todoList)); // Save todoList to localStorage
   }, [todoList]); // Adding todoList as a dependency
 
+  return [todoList, setTodoList]
+
+};
+
+function App() {
+  const [todoList, setTodoList] = useSemiPersistentState();
+  const addTodo = (newTodo) => {
+    setTodoList(prevTodoList => [...prevTodoList, newTodo]);
+  };
   return (
     <div>
       <h1> Todo List </h1>
